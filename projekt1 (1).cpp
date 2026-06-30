c2189c9286a59dde261a17a619b4b3368c963abb
marcel6315 <wujos631@gmail.com>
2026-06-30 19:39:24 +0200

    Update projekt1 (1).cpp

--- projekt1 (1).cpp
+++ projekt1 (1).cpp
@@ -2,18 +2,63 @@
 
 using namespace std;
 
-int main() {
+
+bool czyPierwsza(int n)
+{
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
     int a, b;
-    cout << "Podaj liczbe calkowita ";
+    cout << "Podaj dwie liczby calkowite: ";
     cin >> a >> b;
+
     int wyjscie;
-    do {
-        cout << endl;
+
+    do
+    {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "1. Sprawdz czy liczba jest pierwsza" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
-    } while(wyjscie != 0);
+
+        switch (wyjscie)
+        {
+        case 1:
+        {
+            int liczba;
+            cout << "Podaj liczbe: ";
+            cin >> liczba;
+
+            if (czyPierwsza(liczba))
+                cout << "Liczba jest pierwsza." << endl;
+            else
+                cout << "Liczba nie jest pierwsza." << endl;
+
+            break;
+        }
+
+        case 0:
+            cout << "Koniec programu." << endl;
+            break;
+
+        default:
+            cout << "Niepoprawny wybor." << endl;
+        }
+
+    } while (wyjscie != 0);
+
     return 0;
 }
